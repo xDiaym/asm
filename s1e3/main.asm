038cb23f302b7b4f834a8ff7497213e8e6d6f125
Matthew Nekirov <matthew.nekirov@gmail.com>
2024-03-17 16:13:51 +0300

    add s1e3

--- s1e3/main.asm
+++ s1e3/main.asm
@@ -9,18 +9,23 @@ digit_sum:
     mov     rbp, rsp
     sub     rsp, 8
 
-    ; rdi - 
-    mov     rbx, 10
-    mov     rax, rdi
+    ; abs(x) = (x XOR y) - y, y = x >> 31
+    mov     eax, edi
+    sar     eax, 31
+    xor     edi, eax
+    sub     edi, eax
+
+    mov     ebx, 10
+    mov     eax, edi
     mov     qword [rbp-8], 0
     .loop:
-    mov     rdx, 0
-    div     rbx
-    add     [rbp-8], rdx
-    test    rax, rax
+    mov     edx, 0
+    div     ebx
+    add     [rbp-8], edx
+    test    eax, eax
     jnz     .loop
 
-    mov     rax, [rbp-8]
+    mov     eax, [rbp-8]
     add     rsp, 8
     pop     rbx
     pop     rbp
@@ -36,26 +41,46 @@ _start:
     mov     [rbp-8], rax
 
     mov     rdi, [rbp-8]
-    mov     rsi, read2_fmt
+    mov     rsi, print_fmt
     lea     rdx, [rbp-12]
     call    fscanf
 
+    mov     dword [rbp-20], 0  ; A_i, such digit_sum(A_i) > digit_sum(A_j), j=1..i
+    mov     dword [rbp-24], 0  ; digit_sum(A_i)
     .iter:
         mov     rdi, [rbp-8]
-        mov     rsi, read2_fmt
+        mov     rsi, scan_fmt
         lea     rdx, [rbp-16]
         call    fscanf
 
-        mov     rdi, read1_fmt
-        mov     esi, [rbp-16]
-        call    printf
+        mov     edi, [rbp-16]
+        call    digit_sum
+        mov     [rbp-28], eax
 
+        cmp     [rbp-24], eax
+        jl      .update
+        jne     .ignore_update
+        mov     eax, [rbp-16]
+        cmp     [rbp-20], eax
+        jl      .update
+        jmp     .ignore_update
+
+        .update:
+        mov     [rbp-20], eax
+        mov     eax, [rbp-28]
+        mov     [rbp-24], eax
+
+        .ignore_update:
         dec     dword [rbp-12]
         cmp     dword [rbp-12], 0
         jnz      .iter
 
     mov     rdi, [rbp-8]
     call    fclose
+
+    mov     rdi, print_fmt
+    mov     esi, [rbp-20]
+    call    printf
 
     mov     rax, sys_exit
     xor     rdi, rdi
@@ -67,5 +92,5 @@ input_file          db "input.txt", 0
 input_file_mode     db "r", 0
 output_file         db "output.txt", 0
 output_file_mod     db "w", 0
-read1_fmt           db "%i", 10, 0
-read2_fmt           db "%i", 0
+print_fmt           db "%i", 10, 0
+scan_fmt           db "%i", 0
