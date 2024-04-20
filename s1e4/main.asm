a95c1d395fad51f1ca3dfbd0b43b3e8bd455f31e
Matthew Nekirov <matthew.nekirov@gmail.com>
2024-04-20 21:05:02 +0300

    s2e4: add strcspn

--- s1e4/main.asm
+++ s1e4/main.asm
@@ -83,6 +83,93 @@ asm_strspn:
     pop     rbp
     ret
 
+;; https://github.com/bminor/musl/blob/master/src/string/strcspn.c
+global asm_strcspn
+asm_strcspn:
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 256
+
+    mov     qword [rbp-256], 0
+    mov     qword [rbp-248], 0
+    mov     qword [rbp-240], 0
+    mov     qword [rbp-232], 0
+    mov     qword [rbp-224], 0
+    mov     qword [rbp-216], 0
+    mov     qword [rbp-208], 0
+    mov     qword [rbp-200], 0
+    mov     qword [rbp-192], 0
+    mov     qword [rbp-184], 0
+    mov     qword [rbp-176], 0
+    mov     qword [rbp-168], 0
+    mov     qword [rbp-160], 0
+    mov     qword [rbp-152], 0
+    mov     qword [rbp-144], 0
+    mov     qword [rbp-136], 0
+    mov     qword [rbp-128], 0
+    mov     qword [rbp-120], 0
+    mov     qword [rbp-112], 0
+    mov     qword [rbp-104], 0
+    mov     qword [rbp-96], 0
+    mov     qword [rbp-88], 0
+    mov     qword [rbp-80], 0
+    mov     qword [rbp-72], 0
+    mov     qword [rbp-64], 0
+    mov     qword [rbp-56], 0
+    mov     qword [rbp-48], 0
+    mov     qword [rbp-40], 0
+    mov     qword [rbp-32], 0
+    mov     qword [rbp-24], 0
+    mov     qword [rbp-16], 0
+    mov     qword [rbp-8], 0
+
+    mov     r12, rdi  ;; save start of src strting
+
+    test     rsi, rsi
+    jz      .empty_src
+
+    ;; fill char mask
+    .mask_fill:
+    movsx   r8, byte [rsi]
+    test    r8, r8
+    jz      .mask_check
+    or      byte [rsp+r8], 1
+    inc     rsi
+    jmp     .mask_fill
+
+    .mask_check:
+    movsx   r8, byte [rdi]
+    test    r8, r8
+    jz      .cmp_end
+    movsx   r9, byte [rsp+r8]
+    test    r9, r9
+    jnz      .cmp_end
+    inc     rdi
+    jmp     .mask_check
+
+
+    .cmp_end:
+    mov     rax, rdi
+    sub     rax, r12
+    jmp     .end
+
+    .empty_src:
+    mov     r8b, [rdi]
+    test    r8b, r8b
+    jz      .empty_src_end
+    inc     rdi
+    jmp     .empty_src
+
+    .empty_src_end:
+    mov     rax, rdi
+    sub     rax, r12
+
+    .end:
+    add     rsp, 256
+    pop     rbp
+    ret
+
+
 asm_strtok:
     push    rbp
     mov     rbp, rsp
--- s1e4/test.c
+++ s1e4/test.c
@@ -5,11 +5,19 @@
 extern char* asm_strtok(char *str, const char *delim);
 
 extern size_t asm_strspn(const char* dst, const char* src);
+extern size_t asm_strcspn(const char* dst, const char* src);
 
 int main() {
   assert(asm_strspn("hello world", "hleo") == 5);
   assert(asm_strspn("hello world", NULL) == 0);
   assert(asm_strspn("hhhhh world", "h") == 5);
+
+  assert(asm_strcspn("hello world", "word") == 4);
+  assert(asm_strcspn("hello world", NULL) == 11);
+  assert(asm_strcspn("hhhhh world", "h") == 0);
+  assert(asm_strcspn("world hhhhh", "h") == 6);
+
+
 
   assert(asm_strtok(NULL, NULL) == NULL);
   return 0;
